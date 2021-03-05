242504f65ba71417dfa52a1eb1aeb21c2a50296c
thecrazyapple01 <65122166+thecrazyapple01@users.noreply.github.com>
2021-03-05 18:37:42 +0200

    Creation of someting beautiful
    Co-authored-by: Sarissa Aurori <saurori@tudelft.nl>
    Co-authored-by: Stefano Kok <srkok@tudelft.nl>
    Co-authored-by: sieleoilee@hotmail.com <sieleoilee@hotmail.com>
    Co-authored-by: 2637438364@qq.com <2637438364@qq.com>
    Co-authored-by: Florens Petersen <fcdpetersen@tudelft.nl>
    Co-authored-by: Matthijs Duursma <mduursma@tudelft.nl>

--- main.py
+++ main.py
@@ -1,1 +1,60 @@
-print("Hello hell!")
+import numpy as np
+print("Hello bitches!")
+print('hi')
+print('im here')
+i = 0 
+while i < 100:
+    print(i)
+    i += 1
+
+hello = 1934
+k=0
+while k<10:
+    print("Hello bitchazzz") 
+    k+=1
+
+# I really don't know anything
+# yes this is a very good idea
+#    i -= 1 #now the question is, is this going to keep on going infinitely? hahahha
+# oh no poor andreis computer i really hope he isnt going to run this
+# btw it wont run infini#tewlvnalksaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+#why not?
+#oh wait
+# i think it will i am dumb yes hmmm im just not sure what it will do if you go below 0  hmmmm yes maybe
+# im not sure if florens has numpy imported yet tho not florens andrei i am having a hard time
+# well thats a fair point.
+# we can also make a new variable and not change that one so it will never exceed a certain value, easy way to break andreis computer
+#hahahhha , do it ahha
+#beautiful if you ask me okay imma do the logbook
+#just splendid
+#only i should be k or huh yeah me too
+# no it is just doing minus i but that doesnt matter
+#but then it does print it once right
+#now i think it will not have any problem
+#yeah agreed
+#logbook time yaaaas
+#uhm maybe then it should be np.arange(1,hello), maybe then it would run infinitely
+#hahahhahahhhahaha #dont delete this line, it is really important
+    
+#really hard code i am very proud of this
+#when everyone jokes about hard code and you just die inside heheheh, 
+# people dont have to joke about hard code for me to die inside
+# that is okay we will have a blast here
+
+ #whwhwhhwahahahh, i feel better now 
+
+ # that is good,  i likehaving conversations like this
+ #yeah agree, feels like going back 30 years :
+ # oh my god how is that thing called on the ds OH wait pictochat that was amazing
+#wowww totally forgot about that hahah, that was the real deal
+# holy shit that was cool
+# i really feel like playing mario kart now or nintendogs
+#lets do it a teambuilding thingy hahah or mario party
+ #g olden days
+ # extremely good idea i dont see any objections
+ #can you code a team building planning thing hahah
+ #wait what do i have to code
+ # a sort of datum prikker heheh
+ # i am not that good with interfaces
+
+ #HUH?
