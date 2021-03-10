89d5615403fb1f78f51a6f3a754502e8fcb88293
thecrazyapple01 <65122166+thecrazyapple01@users.noreply.github.com>
2021-03-10 15:17:15 +0200

    Update visualisation.py

--- visualisation.py
+++ visualisation.py
@@ -1,25 +1,40 @@
+import numpy as np
+
 # Algorithm for the visualisation of the 3D positions of the paticles in time
 
 file_no = input("Select .dat file to import [integer between 0 and 2]: ")
 
 f = open("data\Case" + str(file_no) + ".dat", 'r')
 
+data = np.zeros((5001, 20001, 16))  # (snapshot, trackID, values)
+times = np.array([]) 
 snapshot = -1
 no_part = 0
 time = -1
+i = 0
 
 for line in f.readlines():
-    if line.startswith("TITLE") or line.startswith("VARIABLES") or line.startswith("DATAPACKING"):
+    if line.startswith("TITLE") or line.startswith("VARIABLES") or line.startswith("DATAPACKING") or line.startswith("I="):
         pass
     if line.startswith("ZONE"):
-        # snapshot = int(line[17:20]) <- to be fixed with str.split(sep)
+        snapshot = int(line.split(' ')[2][:-2])
+        print(f"Snapshot = {snapshot}")
         pass
     if line.startswith("STRANDID"):
-        # time = float(line[26:]) <- to be fixed with str.split(sep)
-        print(time)
+        time = float(line.split('=')[2])
+        times = np.append(times, time)
+        print(f"Time = {time}")
+        pass
+    values = line.split(' ')
+    # print(values)
+    print(len(values))
+    # id = values[8] # trackID
+
         
 
 f.close()
+
+print(times)
 
 a = "house"
 print(a[2:-1])
