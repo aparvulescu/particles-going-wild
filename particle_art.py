7d81f9f78041fbb7d6eaa6eaa2b3c5b662ba7fb8
thecrazyapple01 <65122166+thecrazyapple01@users.noreply.github.com>
2021-03-10 18:08:43 +0200

    Co-authored-by: Sarissa Aurori <saurori@tudelft.nl> Co-authored-by: Matthijs Duursma <mduursma@tudelft.nl> Co-authored-by: Jiaxuan CHEN <jiaxuanchen@tudelft.nl> Co-authored-by: Stefano Kok <srkok@tudelft.nl> Co-authored-by: Florens Petersen <fcdpetersen@tudelft.nl> Co-authored-by: sieleoilee@hotmail.com <sieleoilee@hotmail.com>

---
+++ particle_art.py
@@ -0,0 +1,82 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as sp
+from scipy import interpolate, integrate
+
+import numpy as np
+
+# Algorithm for the visualisation of the 3D positions of the paticles in time
+
+file_no = input("Select .dat file to import [integer between 0 and 2]: ")
+
+f = open("data\Case" + str(file_no) + ".dat", 'r')
+import numpy as np
+
+# Algorithm for the visualisation of the 3D positions of the paticles in time
+
+file_no = input("Select .dat file to import [integer between 0 and 2]: ")
+
+f = open("data\Case" + str(file_no) + ".dat", 'r')
+
+data = np.zeros((5001, 20001, 16))  # (snapshot, trackID, values)
+times = np.array([])
+snapshot = -1
+no_part = 0
+time = -1
+
+
+for line in f.readlines():
+    if line.startswith("TITLE") or line.startswith("VARIABLES") or line.startswith("DATAPACKING") or line.startswith(
+            "I="):
+        continue
+    if line.startswith("ZONE"):
+        snapshot = int(line.split(' ')[2][:-2])
+        # print(f"Snapshot = {snapshot}")
+        continue
+    if line.startswith("STRANDID"):
+        time = float(line.split('=')[2])
+        times = np.append(times, time)
+        # print(f"Time = {time}")
+        continue
+    values = line.split(' ')
+    tid = int(values[8])  # trackID
+#    print(tid)
+    data[snapshot][tid][0] = values[0]  # x-position
+    data[snapshot][tid][1] = values[1]  # y-position
+    data[snapshot][tid][2] = values[2]  # z-position
+    data[snapshot][tid][3] = values[4]  # x-velocity (u)
+    data[snapshot][tid][4] = values[5]  # y-velocity (v)
+    data[snapshot][tid][5] = values[6]  # z-velocity (w)
+    data[snapshot][tid][6] = values[7]  # Speed magnitude (|V|)
+    data[snapshot][tid][7] = values[9]  # x-acceleration (ax)
+    data[snapshot][tid][8] = values[10]  # y-acceleration (ay)
+    data[snapshot][tid][9] = values[11]  # z-acceleration (az)
+    data[snapshot][tid][10] = values[12]  # acceleration magnitude (|a|)
+    data[snapshot][tid][11] = values[8]     #
+
+f.close()
+
+
+
+parameter = input('Which parameter do you want to observe? (e.g.: x, y, u,v )')
+track = int(input('Which particle do you want to track?'))
+para_lst = [ 'x' ,'y','z','u','v','w', 'V','ax','ay','az','a']
+parameter = para_lst.index(str(parameter))
+
+i = 0
+y_value_lst = []
+time_lst = []
+while i < 5001:
+    if data[i][track][0] < int(-1):
+        y_value_lst.append(data[i][track][parameter])
+        time_lst.append(times[i])
+        i += 1
+    else:
+        i += 1
+
+
+plt.plot(time_lst, y_value_lst , 'o-')
+plt.xlabel('time [s]')
+plt.ylabel(para_lst[parameter])
+
+plt.show()
--- timestamp_vis.py
+++ timestamp_vis.py
@@ -1,5 +1,9 @@
+
+
 from data_gathering import get_data
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import numpy as np
 
 snapshot = 0
@@ -11,11 +15,20 @@ z = np.array([])
 data_now = get_data(1)
 
 for particle in data_now[snapshot]:
-    x = np.append(x, particle[0])
-    y = np.append(y, particle[1])
-    z = np.append(z, particle[2])
+    if particle[0] != 0 and particle[1] != 0 and particle[2] != 0:
+        x = np.append(x, particle[0])
+        y = np.append(y, particle[1])
+        z = np.append(z, particle[2])
 
-plt.plot(x, y, z)
+
+# plt.plot(x, y, z)
+# plt.show()
+
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+
+ax.plot3D(x, y, z, 'o')
+
 plt.show()
-
 
