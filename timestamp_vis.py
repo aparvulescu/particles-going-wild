d004daea590fce0e33e5721e378ce5b6f7475d81
thecrazyapple01 <65122166+thecrazyapple01@users.noreply.github.com>
2021-03-12 18:13:33 +0200

    Some session
    Co-authored-by: Jiaxuan CHEN <jiaxuanchen@tudelft.nl>
    Co-authored-by: Sarissa Aurori <saurori@tudelft.nl>
    Co-authored-by: sieleoilee@hotmail.com <sieleoilee@hotmail.com>
    Co-authored-by: Stefano Kok <srkok@tudelft.nl>
    Co-authored-by: Matthijs Duursma <mduursma@tudelft.nl>
    Co-authored-by: Florens Petersen <fcdpetersen@tudelft.nl>

--- timestamp_vis.py
+++ timestamp_vis.py
@@ -6,29 +6,50 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 
-snapshot = 0
-file_no = 1
 x = np.array([])
 y = np.array([])
 z = np.array([])
 
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ln, = ax.scatter(x[0], y[0], z[0], 'o')
+
+
+pos = []
+snapshot = 0
+file_no = 1
+
+
 data_now = get_data(1)
 
-for particle in data_now[snapshot]:
-    if particle[0] != 0 and particle[1] != 0 and particle[2] != 0:
-        x = np.append(x, particle[0])
-        y = np.append(y, particle[1])
-        z = np.append(z, particle[2])
+for snapshot in range(len(data_now)):
+    pos_now = []
+    for particle in data_now[snapshot]:
+        if particle[0] != 0 and particle[1] != 0 and particle[2] != 0:
+            pos_now.append([particle[0], particle[1], particle[2]])
+            #x = np.append(x, particle[0])
+            #y = np.append(y, particle[1])
+            #z = np.append(z, particle[2])
+    pos.append(pos_now)
+
+def update(frame):
+    x = np.append(x, frame)
+    y = np.append(y, np.sin(frame))
+    z = np.append(z, np.cos(frame))
 
 
-# plt.plot(x, y, z)
-# plt.show()
 
 
-fig = plt.figure()
-ax = plt.axes(projection='3d')
 
-ax.plot3D(x, y, z, 'o')
+
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+ax.set_zlabel("Z")
+
+ani = animation.FuncAnimation(fig, update, 100)
 
 plt.show()
+
+
+
 
