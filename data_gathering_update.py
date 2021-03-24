57d2769aaa9f0a1bb4f2f0c6bced00457257620f
thecrazyapple01 <65122166+thecrazyapple01@users.noreply.github.com>
2021-03-24 18:29:30 +0200

    End of session 13
    Co-authored-by: sieleoilee@hotmail.com <sieleoilee@hotmail.com>
    Co-authored-by: Sarissa Aurori <saurori@tudelft.nl>
    Co-authored-by: Florens Petersen <fcdpetersen@tudelft.nl>
    Co-authored-by: Jiaxuan CHEN <jiaxuanchen@tudelft.nl>

---
+++ data_gathering_update.py
@@ -0,0 +1,71 @@
+import numpy as np
+import re
+
+
+# Algorithm for the visualisation of the 3D positions of the paticles in time
+
+def get_data(file_no):
+    global values, tid, track_id_per_snapshot
+    f = open("data\Case" + str(file_no) + ".dat", 'r')
+
+    data = np.zeros((5001, 20001, 11))  # (snapshot, trackID, values)
+    times = np.array([])
+    max_snapshot = [100, 3626, 3626]
+    track_id_per_snapshot = np.zeros((max_snapshot[file_no], 70))
+    grid = np.full_like(track_id_per_snapshot, -1)
+    #track_id_per_snapshot_00 = np.zeros(max_snapshot[file_no])
+    snapshot = -1
+    no_part = 0
+    time = -1
+    i = 0
+
+    for line in f.readlines():
+        if line.startswith("TITLE") or line.startswith("VARIABLES") or line.startswith(
+                "DATAPACKING"):
+            i = 0
+            continue
+        if line.startswith("ZONE"):
+            snapshot = int(line.split(' ')[2][:-2])
+            # print(f"Snapshot = {snapshot}")
+            continue
+        if line.startswith("STRANDID"):
+            time = float(line.split('=')[2])
+            times = np.append(times, time)
+            # print(f"Time = {time}")
+            continue
+        if line.startswith("I"):
+            intermediate = str(line.split('=')[1])
+            I = int(intermediate[0:2])
+            continue
+        values = line.split(' ')
+        tid = int(values[8])  # trackID
+        data[snapshot][tid][0] = values[0]  # x-position
+        data[snapshot][tid][1] = values[1]  # y-position
+        data[snapshot][tid][2] = values[2]  # z-positionx
+        data[snapshot][tid][3] = values[4]  # x-velocity (u)
+        data[snapshot][tid][4] = values[5]  # y-velocity (v)
+        data[snapshot][tid][5] = values[6]  # z-velocity (w)
+        data[snapshot][tid][6] = values[7]  # Speed magnitude (|V|)
+        data[snapshot][tid][7] = values[9]  # x-acceleration (ax)
+        data[snapshot][tid][8] = values[10]  # y-acceleration (ay)
+        data[snapshot][tid][9] = values[11]  # z-acceleration (az)
+        data[snapshot][tid][10] = values[12]  # acceleration magnitude (|a|)
+
+        track_id_per_snapshot[snapshot][i] = tid
+        i += 1
+    f.close()
+    #for n in range(max_snapshot[file_no]):
+        #track_id_per_snapshot = np.trim_zeros([track_id_per_snapshot_00[n]], 'b')
+        #track_id_per_snapshot[n] = np.append(track_id_per_snapshot_zeros)
+    #p = np.where(track_id_per_snapshot_00 != 0)
+    #track_id_per_snapshot_00 = track_id_per_snapshot_00[min(p[1]): max(p[1]) + 1]
+    return data, values, track_id_per_snapshot
+
+
+# file_no = input("Select .dat file to import [integer between 0 and 2]: ")
+#track_id_per_snapshot = get_data(0)[2]
+
+
+
+
+# print(times)
--- trackid_gridupdate.py
+++ trackid_gridupdate.py
@@ -1,9 +1,107 @@
-from data_gathering import get_data
 import numpy as np
+from data_gathering_update import get_data
+from time import clock
 
+#stopwatch
+start_time = time.time()
+
+#which file do you want to consider?
 file_no = 0
+max_snapshot = [100, 3626, 3626]
+dt_arr = [0.014, 0.001, 0.001]  # s? to check Case1.dat and Case2.dat
+dt = dt_arr[file_no]
 
-data_now = get_data(file_no)
+#import from get_Data
+data = get_data(file_no)[0]
+values = get_data(file_no)[1]
+track_id_per_snapshot = get_data(file_no)[2]
 
-print("I am not a human") #whoever wrote this, are you okay?
+#empty marker tid list
+marker_tid_list = []  # change to list
+marker_last_seen_snp = []
 
+#marker_tid_list.append("first tid for new marker")
+#marker_tid_list["index of marker"].append("new_tid")
+
+#choose radius (mm)
+r = 10
+
+#open file to write in
+track_id_file = open("TrackIDupdate.dat")
+
+for trackid in track_id_per_snapshot[0]:
+    if trackid == -1:
+        continue
+    marker_tid_list.append(trackid)
+    marker_last_seen_snp.append(0)
+
+
+for snapshot in range(1, max_snapshot[file_no]):
+    # for marker in marker_tid_list:
+        #print("marker", marker_no)
+        for n in range(70):  # change to list
+            a = int(track_id_per_snapshot[snapshot][n])
+            if a == -1:
+                continue
+            if a in marker_tid_list[:][-1]: 
+                marker_last_seen_snp[marker_tid_list[:][-1].index(a)] = snapshot
+                continue
+            point_coor = np.array([data[snapshot][a][0], data[snapshot][a][1], data[snapshot][a][2]])
+            
+            ok = False 
+            for marker, marker_no in enumerate(marker_tid_list):
+                snapshot_old = marker_last_seen_snp[marker_no]
+                b = marker[-1]  # last_tid_marker
+                time_ago = snapshot - snapshot_old
+
+                origin_coor = np.array([data[snapshot_old][b][0], data[snapshot_old][b][1], data[snapshot_old][b][2]])
+                #pythagoras_origin = np.linalg.norm(origin_coor)
+                origin_vel = np.array([data[snapshot_old][b][3], data[snapshot_old][b][4], data[snapshot_old][b][5]])
+                origin_acc = np.array([data[snapshot_old][b][7], data[snapshot_old][b][8], data[snapshot_old][b][9]])
+                #predicted_vel = origin_vel + dt * origin_acc
+                predicted_coor = origin_coor + dt * origin_vel + 0.5*dt*dt * origin_acc
+
+                # x = x0 + v0*t + 0.5*a*t^2
+                distance = abs(np.linalg.norm(point_coor - predicted_coor))
+                if distance < r:
+                 #np.append(marker_tid_list[marker_no], b_array)
+                    marker_tid_list[marker_no].append(a)
+                    marker_last_seen_snp[marker_no] = snapshot
+                    ok = True
+                    break
+            
+            if ok == True:
+                continue
+            else:
+                marker_tid_list.append(a)
+                marker_last_seen_snp.append(snapshot)
+
+
+'''
+        if snapshot != 0:
+            origin_coor = np.array([data[snapshot][b][0], data[snapshot][b][1], data[snapshot][b][2]])
+            # pythagoras_origin = np.linalg.norm(origin_coor)
+            origin_vel = np.array([data[snapshot][b][3], data[snapshot][b][4], data[snapshot][b][5]])
+            origin_acc = np.array([data[snapshot][b][7], data[snapshot][b][8], data[snapshot][b][9]])
+            predicted_vel = origin_vel + dt * origin_acc
+            predicted_coor = origin_coor + dt * predicted_vel
+            for m in range(70):
+                c = int(track_id_per_snapshot[snapshot + 1][m])
+                point_coor = np.array([data[snapshot + 1][c][0], data[snapshot + 1][c][1], data[snapshot + 1][c][2]])
+                distance = np.linalg.norm(point_coor - predicted_coor)
+                if distance < r:
+                    #np.append(marker_tid_list[marker_no], b_array)
+                    marker_tid_list[marker_no].append(c)
+'''      
+
+print(marker_tid_list)
+print("--- %s seconds ---" % (time.time() - start_time))
+
+# SUMMARY:
+# goal; update initial grid with new track-IDs for each snapshot
+# for each track-ID in initial snapshot t_0 look for most close neighbour in both x,y,z directions in snapshot t_1
+# add some conditions when the found track-ID is not the closest in all three directions
+# assign the found track-ID to the grid
+
+# CRITICAL POINTS:
+# when a marker is found that wasn't in the grid earlier what to do???
